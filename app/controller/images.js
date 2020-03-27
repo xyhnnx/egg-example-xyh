1fef660f22cd629260f3d3dc1f6c9a3367ebdb11
xingyanhai <142xyh753869>
2020-03-27 15:13:58 +0800

    百度图片搜索

--- app/controller/images.js
+++ app/controller/images.js
@@ -6,11 +6,14 @@ class NewsController extends Controller {
   async list() {
     const ctx = this.ctx;
     // 示例：请求一个 npm 模块信息
-    const result = await ctx.curl(`https://image.baidu.com/search/acjson?word=${ctx.query.search}&tn=resultjson_com&ipn=rj&ct=201326592&is=&fp=result&cl=2&lm=-1&ie=utf-8&oe=utf-8&adpicid=&st=-1&z=&ic=0&hd=&latest=&copyright=&s=&se=&tab=&width=&height=&face=0&istype=2&qc=&nc=1&fr=&expermode=&force=&pn=180&rn=30&gsm=b4&1585279645461=`, {
+    const result = await ctx.curl('https://image.baidu.com/search/acjson?tn=resultjson_com&ipn=rj&ct=201326592&is=&fp=result&cl=2&lm=-1&ie=utf-8&oe=utf-8&adpicid=&st=-1&z=&ic=0&hd=&latest=&copyright=&s=&se=&tab=&width=&height=&face=0&istype=2&qc=&nc=1&fr=&expermode=&force=&pn=180&rn=30&gsm=b4&1585279645461=', {
       // 自动解析 JSON response
       dataType: 'json',
       // 3 秒超时
       timeout: 3000,
+      data: {
+        word: ctx.query.search,
+      },
     });
     await this.ctx.render('images/list.tpl', { list: result.data.data });
   }

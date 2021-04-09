77f57bb7132bf351ebebfe66483b02e0a3871582
1101099728@qq.com <142xyh753869>
2021-04-09 09:39:36 +0800

    zhihu.js

--- app/methods/zhihu.js
+++ app/methods/zhihu.js
@@ -16,50 +16,77 @@ class Zhihu {
         let outputDir = this.outputDir
         makeDir(outputDir)
         makeDir(outputDir + '/essenceAnswer')
-        console.log('getEssenceData-start')
-        let err = []
+        let apiArrAll = []
+        let count = 1 // 并发数量
         for (let i = 0; i < topicsData.length; i++) {
             let id = topicsData[i].split('/')[2]
-            if (fs.existsSync(`${outputDir}/essenceAnswer/essenceAnswer_${id}.json`)) {
-                console.log(`文件已存在：essenceAnswer_${id}.json`)
-                continue
-            }
-            let essenceData = []
-            let flag = true
-            let url
-            while (flag) {
-                console.log(`id = ${id},count=${essenceData.length}  ${i}/${topicsData.length}`)
-                let res
-                for (let tryCount = 0; tryCount < 3; tryCount++) {
-                    try {
-                        res = await this.getEssence(url, id)
-                        break
-                    } catch (e) {
-                        console.log(`getEssence第${tryCount + 1}次尝试-${url}---${e}`)
-                        if (tryCount === 2) {
-                            err.push({id: id})
-                            await fs.writeFileSync(`${outputDir}/essenceData_error.json`, JSON.stringify(err))
-                        }
-                        await timeout()
-                    }
-                }
-                if (res) {
-                    essenceData.push(...res.data)
-                    url = res.nextUrl
-                } else {
-                    flag = false
-                }
-                await timeout(10 + Math.random() * 20)
-            }
+            apiArrAll.push(() => {
+                return this.getEssenceItemData(id)
+            })
+        }
 
-            await fs.writeFileSync(`${outputDir}/essenceAnswer/essenceAnswer_${id}.json`, JSON.stringify(essenceData))
-            console.log(`essenceAnswer_${id}.json 写入成功！count:${essenceData.length}`)
+        while (apiArrAll.length) {
+            console.log(`------------剩余：${apiArrAll.length} 总量：${topicsData.length}------------------`)
+            await Promise.all(apiArrAll.splice(0, count).map(e => e()))
         }
         console.log(`-------------------end write------------------------`)
     }
 
+    async getEssenceItemData (id) {
+        let outputDir = this.outputDir
+        if (fs.existsSync(`${outputDir}/essenceAnswer/essenceAnswer_${id}.json`)) {
+            let data = fs.statSync(`${outputDir}/essenceAnswer/essenceAnswer_${id}.json`)
+            if (data.size < 1000) {
+                console.log(`文件已存在但是是空的：essenceAnswer_${id}.json`)
+            } else {
+                console.log(`文件已存在：essenceAnswer_${id}.json`)
+                return
+            }
+        }
+
+        let essenceData = []
+        let flag = true
+        let url
+        let isError = false
+        while (flag) {
+            console.log(`id = ${id},count=${essenceData.length}`)
+            let res
+            for (let tryCount = 0; tryCount < 3; tryCount++) {
+                try {
+                    res = await this.getEssence(url, id)
+                    break
+                } catch (e) {
+                    console.log(`getEssence第${tryCount + 1}次尝试-${url}---${e}`)
+                    if (tryCount === 2) {
+                        let err = []
+                        try {
+                            err = JSON.parse(fs.readFileSync(`${outputDir}/essenceData_error.json`, 'utf-8'))
+                        } catch (e) {
+                            console.log('essenceData_error.json ', e)
+                        }
+                        err.push({id: id})
+                        await fs.writeFileSync(`${outputDir}/essenceData_error.json`, JSON.stringify(err))
+                        isError = true
+                    }
+                    await timeout()
+                }
+            }
+            if (res) {
+                essenceData.push(...res.data)
+                url = res.nextUrl
+            } else {
+                flag = false
+            }
+            await timeout(10 + Math.random() * 20)
+        }
+        if(essenceData.length && !isError) {
+            await fs.writeFileSync(`${outputDir}/essenceAnswer/essenceAnswer_${id}.json`, JSON.stringify(essenceData))
+            console.log(`essenceAnswer_${id}.json 写入成功！count:${essenceData.length}`)
+        }
+    }
+
     async getEssence (url, id) {
-        url = url || `https://www.zhihu.com/api/v4/topics/${id}/feeds/essence?include=data%5B%3F(target.type%3Dtopic_sticky_module)%5D.target.data%5B%3F(target.type%3Danswer)%5D.target.content%2Crelationship.is_authorized%2Cis_author%2Cvoting%2Cis_thanked%2Cis_nothelp%3Bdata%5B%3F(target.type%3Dtopic_sticky_module)%5D.target.data%5B%3F(target.type%3Danswer)%5D.target.is_normal%2Ccomment_count%2Cvoteup_count%2Ccontent%2Crelevant_info%2Cexcerpt.author.badge%5B%3F(type%3Dbest_answerer)%5D.topics%3Bdata%5B%3F(target.type%3Dtopic_sticky_module)%5D.target.data%5B%3F(target.type%3Darticle)%5D.target.content%2Cvoteup_count%2Ccomment_count%2Cvoting%2Cauthor.badge%5B%3F(type%3Dbest_answerer)%5D.topics%3Bdata%5B%3F(target.type%3Dtopic_sticky_module)%5D.target.data%5B%3F(target.type%3Dpeople)%5D.target.answer_count%2Carticles_count%2Cgender%2Cfollower_count%2Cis_followed%2Cis_following%2Cbadge%5B%3F(type%3Dbest_answerer)%5D.topics%3Bdata%5B%3F(target.type%3Danswer)%5D.target.annotation_detail%2Ccontent%2Chermes_label%2Cis_labeled%2Crelationship.is_authorized%2Cis_author%2Cvoting%2Cis_thanked%2Cis_nothelp%2Canswer_type%3Bdata%5B%3F(target.type%3Danswer)%5D.target.author.badge%5B%3F(type%3Dbest_answerer)%5D.topics%3Bdata%5B%3F(target.type%3Danswer)%5D.target.paid_info%3Bdata%5B%3F(target.type%3Darticle)%5D.target.annotation_detail%2Ccontent%2Chermes_label%2Cis_labeled%2Cauthor.badge%5B%3F(type%3Dbest_answerer)%5D.topics%3Bdata%5B%3F(target.type%3Dquestion)%5D.target.annotation_detail%2Ccomment_count%3B&limit=10`
+        url = url || `https://www.zhihu.com/api/v4/topics/${id}/feeds/essence?include=data%5B%3F(target.type%3Dtopic_sticky_module)%5D.target.data%5B%3F(target.type%3Danswer)%5D.target.content%2Crelationship.is_authorized%2Cis_author%2Cvoting%2Cis_thanked%2Cis_nothelp%3Bdata%5B%3F(target.type%3Dtopic_sticky_module)%5D.target.data%5B%3F(target.type%3Danswer)%5D.target.is_normal%2Ccomment_count%2Cvoteup_count%2Ccontent%2Crelevant_info%2Cexcerpt.author.badge%5B%3F(type%3Dbest_answerer)%5D.topics%3Bdata%5B%3F(target.type%3Dtopic_sticky_module)%5D.target.data%5B%3F(target.type%3Darticle)%5D.target.content%2Cvoteup_count%2Ccomment_count%2Cvoting%2Cauthor.badge%5B%3F(type%3Dbest_answerer)%5D.topics%3Bdata%5B%3F(target.type%3Dtopic_sticky_module)%5D.target.data%5B%3F(target.type%3Dpeople)%5D.target.answer_count%2Carticles_count%2Cgender%2Cfollower_count%2Cis_followed%2Cis_following%2Cbadge%5B%3F(type%3Dbest_answerer)%5D.topics%3Bdata%5B%3F(target.type%3Danswer)%5D.target.annotation_detail%2Ccontent%2Chermes_label%2Cis_labeled%2Crelationship.is_authorized%2Cis_author%2Cvoting%2Cis_thanked%2Cis_nothelp%2Canswer_type%3Bdata%5B%3F(target.type%3Danswer)%5D.target.author.badge%5B%3F(type%3Dbest_answerer)%5D.topics%3Bdata%5B%3F(target.type%3Danswer)%5D.target.paid_info%3Bdata%5B%3F(target.type%3Darticle)%5D.target.annotation_detail%2Ccontent%2Chermes_label%2Cis_labeled%2Cauthor.badge%5B%3F(type%3Dbest_answerer)%5D.topics%3Bdata%5B%3F(target.type%3Dquestion)%5D.target.annotation_detail%2Ccomment_count%3B&&limit=10`
         const res = await fetch({
             url,
             headers: {
@@ -182,5 +209,86 @@ class Zhihu {
         }
     }
 }
+
+class ScriptClass {
+    async index() {
+        console.log(process.execPath)
+        console.log(process.cwd())
+        await new Promise((resolve)=>{
+            console.log('xxxxxfsafasfd')
+            setTimeout(() => {
+                console.log('xxxxx')
+                resolve()
+            }, 2000)
+        })
+        try {
+            let jsonList = this.getList()
+            this.json2md(jsonList)
+        } catch (e) {
+            console.log(e)
+        }
+        await new Promise((resolve)=>{
+            console.log('xxxxxfsafasfd')
+            setTimeout(() => {
+                console.log('xxxxx')
+                resolve()
+            }, 10000)
+        })
+
+    }
+    getList() {
+        let list = geFileList(path.join(__dirname, '/essenceAnswer'))
+        let allList = []
+        for(let i = 0;i<list.length;i++) {
+            console.log(`read index = ${i}`)
+            let data = JSON.parse(fs.readFileSync(list[i].path))
+            allList.push(...data)
+        }
+        let newList = allList.map(e => {
+            if(e.target.type === 'article') { // 文章
+                return {
+                    title: e.target.title,
+                    url: e.target.url,
+                    voteupCount: e.target.voteup_count,
+                    commentCount: e.target.comment_count,
+                    updatedTime: e.target.updated,
+                    authorName: e.target.author.name
+                }
+            } else {
+                return {
+                    title: e.target.question.title,
+                    url: `https://www.zhihu.com/question/${e.target.question.id}/answer/${e.target.id}`,
+                    voteupCount: e.target.voteup_count,
+                    commentCount: e.target.comment_count,
+                    updatedTime: e.target.updated_time,
+                    authorName: e.target.author.name
+                }
+            }
+        })
+        newList.sort((a,b)=> b.voteupCount - a.voteupCount)
+        console.log(newList.map(e => e.voteupCount))
+        return newList.slice(0,20)
+        // fs.writeFileSync('./output.json', JSON.stringify(allList))
+    }
+    json2md (json) {
+        let arr = []
+        for (let i = 0; i < json.length; i++) {
+            let item = json[i]
+            let str = `${i + 1}  [${item.title}](${item.url})  
+获赞数： ${String(item.voteupCount).padEnd(10,' ')}   作者： ${item.authorName}  
+    
+    
+      
+  
+`
+            arr.push(str)
+        }
+        fs.writeFileSync(path.join(process.cwd(),'/output.md'), arr.join(''))
+        console.log('.md file read over')
+    }
+}
+
+let s = new ScriptClass()
+// s.index()
 
 module.exports = Zhihu

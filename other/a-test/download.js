431bf2b0b1938c525c0712c294ef65b9312f6599
xyh <1101099728@qq.com>
2021-06-12 21:36:25 +0800

    a-tset

--- other/a-test/download.js
+++ other/a-test/download.js
@@ -3,24 +3,20 @@ const fs = require('fs')
 const path = require('path');
 const fetch = require('../../app/util/fetch')
 const asyncPool = require('tiny-async-pool')
-const distPath = 'I:\\jj-img\\'
+const distPath = 'I:\\a-test\\'
 index();
 
 async function index() {
     let downList = Util.geFileList(distPath)
-    let list = require('./jj-img-map-api.json');
+    let list = require('./list.json');
     let downloadInfoList = []
     let downLoadCount = 0;
     let isError = false
     for (let i = 0; i < list.length; i++) {
         let item = list[i];
-        let obj = {
-            1: 'png',
-            2: 'jpg'
-        }
-        const name = `${item.i}.${obj[item.t]}`
+        const name = item.name + item.downUrl.substring(item.downUrl.lastIndexOf('.'))
         if (!downList.some(e => e.name === name)) {
-            let url = `https://res.zzzmh.cn/origin/${name}`;
+            let url = item.downUrl;
             downloadInfoList.push({
                 url,
                 path: `${distPath}${name}`,
@@ -36,14 +32,14 @@ async function index() {
     未经下载数量: ${list.length - downLoadCount}
     `)
     // 控制下载并发
-    await asyncPool(1, downloadInfoList, async item => {
+    await asyncPool(50, downloadInfoList, async item => {
         try {
             await Util.downloadFileItem(item.url, item.path)
             console.log(`${item.i}.文件下载成功！ ${getTime()}`)
         } catch (e) {
             console.log(`${item.i}.文件下载失败！${e} ${getTime()}`)
             isError = true
-            await Util.timeout(10000)
+            await Util.timeout(500)
         }
     })
     if (isError) {
